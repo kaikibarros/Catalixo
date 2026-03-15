6af693d03c824bab10631157a3dfe9553cc26ab5
WesleyFilhoDev <wesleyfilho221204@gmail.com>
2026-03-15 15:59:19 -0300

    landing page da aplicacao

---
+++ src/frontend/scripts/index.js
@@ -0,0 +1,18 @@
+function revealOnScroll() {
+  const reveals = document.querySelectorAll(".reveal");
+
+  reveals.forEach((element) => {
+    const windowHeight = window.innerHeight;
+    const elementTop = element.getBoundingClientRect().top;
+    const revealPoint = 120;
+
+    if (elementTop < windowHeight - revealPoint) {
+      element.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+
+// roda também ao carregar a página
+revealOnScroll();
