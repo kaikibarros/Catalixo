e74b060be6387d668008c6f362b56178d9404e01
Kaiki Barros <kaikibarros2004@gmail.com>
2026-03-16 22:35:40 -0300

    Feat: dockerizando projeto

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI, UploadFile, File
 from PIL import Image
 from services.garbages_ai import GarbageClassifier
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
 classifier = GarbageClassifier()
 
 @app.get("/")
@@ -18,3 +18,11 @@ async def predict(file: UploadFile = File(...)):
     result = classifier.predict(image)
 
     return result
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], #substituir pelo domínio do frontend
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
